b379080c2c36a77368938eb108f086e71bc05179
jiwon <giwon11292@gmail.com>
2025-05-22 14:53:05 +0900

    2025/05/22

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -1,7 +1,13 @@
 package com.back;
+//git add .
+//git commit -m "work"
+//git push origin main
+
+import com.back.wiseSaying.Step1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Step1 step1 = new Step1();
+        step1.method1();
     }
 }
